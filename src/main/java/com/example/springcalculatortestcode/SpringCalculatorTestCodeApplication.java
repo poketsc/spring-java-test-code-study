376c95e91a8f33888ec4172b55bd7d6e49c0614b
seungchan baek <poketsc@gmail.com>
2023-06-11 22:18:15 +0900

    스프링-자바 테스트 코드 작성 공부

---
+++ src/main/java/com/example/springcalculatortestcode/SpringCalculatorTestCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcalculatortestcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCalculatorTestCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCalculatorTestCodeApplication.class, args);
+    }
+
+}
